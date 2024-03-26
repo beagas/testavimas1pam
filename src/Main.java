ee68f7f88168fb995f9524553aeab9f18f208357
beagas <beatrice.gaskiene@gmail.com>
2024-03-26 14:12:54 +0200

    Successfully resolved name formatting issue

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        String formatedName = formatName("nAglis lapas");
-        assertEquals(formatedName, "Naglis");
+        System.out.println(assertEquals(String.valueOf(formatName("NAGLIS")), "Naglis"));
+        System.out.println(assertEquals(String.valueOf(formatName("nAGLIS")), "Naglis"));
+        System.out.println(assertEquals(String.valueOf(formatName("  naglis ")), "Naglis"));
+        System.out.println(assertEquals(String.valueOf(formatName("  naglis ")), "Naglis"));
+        System.out.println(assertEquals(String.valueOf(formatName("  naglis jonas ")), "Naglis Jonas"));
+        System.out.println(assertEquals(String.valueOf(formatName("  naglis    jonas ")), "Naglis Jonas"));
     }
 
-    public static String formatName(String name) {
-        String actual = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    public static StringBuilder formatName(String name) {
+        String[] names = name.trim().split("\\s+");
+        StringBuilder actual = new StringBuilder();
+        for (String word :names) {
+        actual.append(word.substring(0, 1).toUpperCase()).
+                append(word.substring(1).toLowerCase()).
+                append(" ");
+        }
+        actual = new StringBuilder(actual.substring(0, actual.length() - 1));
         return actual;//suformatuotas Namecase
     }
-
     private static boolean assertEquals(String actual, String expected) {
         if (actual.equals(expected)) {
             System.out.println("Assert passed " + actual + " equals " + expected);
